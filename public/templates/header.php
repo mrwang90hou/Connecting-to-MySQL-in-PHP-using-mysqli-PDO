2f7e96d600c85a60a1860881b7e524dad5a2d124
mrwang90hou <mrwang90hou@163.com>
2018-06-11 11:17:03 +0800

    06-11 commit with Mac

--- public/templates/header.php
+++ public/templates/header.php
@@ -4,9 +4,24 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--原始的代码	<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0" />-->
+<!--    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>-->
 
-	<title>Simple Database App</title>
+
+<!--    向浏览器声明该网页为移动设备自适应网页的meta标签为：-->
+    <meta name="viewport" content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <meta name="format-detection" content="telephone=no">
+    <!--    向浏览器声明该网页为移动设备自适应网页的meta标签为：-->
+
+
+
+    <title>Simple Database App</title>
 
 	<link rel="stylesheet" href="css/style.css">
 </head>
